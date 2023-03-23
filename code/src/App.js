02b17142d9eeca689000f31774892942fb70e953
malvinamaria <mmpolak@protonmail.com>
2023-03-23 17:01:33 +0100

    adding loading and likes logic

--- code/src/App.js
+++ code/src/App.js
@@ -1,33 +1,44 @@
 import React, { useState, useEffect } from 'react';
+import { ThoughtsList } from 'components/ThoughtsList';
+import { ThoughtsForm } from 'components/ThoughtsForm';
+import { MainLoading } from 'components/MainLoading';
 
-import TaskList from 'components/TaskList';
-import TaskForm from 'components/TaskForm';
-
+// eslint-disable-next-line spaced-comment
+//////////////// STATE  VARIABLES & THEIR SETTERS /////////////
+// Declaring variables and setting states
 export const App = () => {
-  const [taskList, setTaskList] = useState([]);
+  const [thoughts, setThoughts] = useState([]);
   const [loading, setLoading] = useState(false);
-  const [newTodo, setNewTodo] = useState('');
+  const [newThought, setNewThought] = useState('');
 
-  const fetchTasks = () => {
+  // eslint-disable-next-line spaced-comment
+  // calling API to get messages from the server in the JSON format
+  // when API is delayed setLoading will show message
+  const fetchThoughts = () => {
     setLoading(true);
-    fetch('https://week-7-backend.onrender.com/tasks')
-      .then((res) => res.json())
-      .then((data) => setTaskList(data))
-      .catch((error) => console.error(error))
+    fetch('https://happy-thoughts-ux7hkzgmwa-uc.a.run.app/thoughts')
+      .then((response) => response.json())
+      .then((data) => setThoughts(data))
+      .catch((error) => console.log(error))
       .finally(() => setLoading(false));
   };
 
+  // Using useEffect() - calling the function fetchThoughts when the component mounts
+  // eslint-disable-next-line react-hooks/rules-of-hooks
   useEffect(() => {
-    fetchTasks();
+    fetchThoughts();
   }, []);
-
-  const handleNewTodoChange = (event) => {
-    setNewTodo(event.target.value);
+  // [] dependancy arrey when empty only called when component is mounted
+  // when new thought is typed we set it to the event
+  const handleNewThoughtChange = (event) => {
+    setNewThought(event.target.value);
   };
 
+  // here we takes object (event) as a parameter plus preventing refreshing page on submition
   const onFormSubmit = (event) => {
     event.preventDefault();
 
+    // ggg
     const options = {
       method: 'POST',
       headers: {
@@ -36,29 +47,59 @@ export const App = () => {
       },
       body: JSON.stringify({
         // eslint-disable-next-line comma-dangle
-        description: newTodo,
+        message: newThought,
         // eslint-disable-next-line comma-dangle
       }),
     };
 
-    fetch('https://week-7-backend.onrender.com/tasks', options)
-      .then((res) => res.json())
-      .then(() => fetchTasks())
-      .finally(() => setNewTodo(''));
+    // here we update fetchFoughts
+    fetch('https://happy-thoughts-ux7hkzgmwa-uc.a.run.app/thoughts', options)
+      .then((response) => response.json())
+      .then(() => fetchThoughts())
+      .finally(() => setNewThought(''));
   };
+  // increase number of likes fo id's thoughts
+  const onLikesIncrease = (LikeID) => {
+    const options = {
+      method: 'POST',
+      headers: {
+        // eslint-disable-next-line comma-dangle
+        'Content-Type': 'application/json',
+        // eslint-disable-next-line comma-dangle
+      },
+    };
+
+    fetch(
+      `https://happy-thoughts-ux7hkzgmwa-uc.a.run.app/thoughts/${LikeID}/like`,
+      options
+    )
+      .then((response) => response.json())
+      .then(console.log('yey it works.'))
+      .catch((error) => console.error(error))
+      .finally(() => fetchThoughts());
+  };
+
+  if (loading) {
+    return <MainLoading />;
+  }
 
   return (
     <div>
-      <TaskForm
-        newTodo={newTodo}
-        onNewTodoChange={handleNewTodoChange}
+      <ThoughtsForm
+        // eslint-disable-next-line no-undef
+        newThought={newThought}
+        // eslint-disable-next-line no-undef
+        handleNewThoughtChange={handleNewThoughtChange}
+        // eslint-disable-next-line no-undef
         onFormSubmit={onFormSubmit}
         // eslint-disable-next-line react/jsx-closing-bracket-location
       />
-      <TaskList
+      <ThoughtsList
+        // eslint-disable-next-line no-undef
         loading={loading}
-        taskList={taskList}
-        setTaskList={setTaskList}
+        // eslint-disable-next-line no-undef
+        thoughts={thoughts}
+        onLikesIncrease={onLikesIncrease}
         // eslint-disable-next-line react/jsx-closing-bracket-location
       />
     </div>
---
+++ code/src/components/MainLoading.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const MainLoading = () => {
+  return (
+    <div className="parent-loading">
+      <div className="loading">
+        <h1>Loading</h1>
+      </div>
+    </div>
+  );
+};
